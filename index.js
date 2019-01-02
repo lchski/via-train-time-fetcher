072228ad2853f6d1ef121bf4ac2341c8c2117e29
Lucas Cherkewski <lucas@ecustom.ca>
2019-01-01 21:00:20 -0500

    how does this stuff work

--- index.js
+++ index.js
@@ -5,5 +5,16 @@ exports.getArrivalDataForDay = async (req, res) => {
 
     const viaResult = await fetch(`http://reservia.viarail.ca/tsi/GetTrainStatus.aspx?l=en&TsiCCode=VIA&TsiTrainNumber=${req.body.trainNumber}&DepartureDate=${req.body.tripDate}&ArrivalDate=${req.body.tripDate}&TrainInstanceDate=${req.body.tripDate}`);
 
-    res.status(200).send(viaResult);
+    console.log(req.body.trainNumber);
+    console.log(req.body.tripDate);
+    console.log(viaResult);
+
+    const resJson = {
+        status: viaResult.status,
+        statusText: viaResult.statusText,
+        url: viaResult.url,
+        body: viaResult.text()
+    };
+
+    res.json(resJson);
 };
